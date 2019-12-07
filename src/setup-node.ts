a0f0ca71e08e9edb6e05ac42d56e3e209897aa1b
Tomotaka Sakuma <ktmtmks@gmail.com>
2019-12-07 17:32:58 +0900

    fix

--- src/setup-node.ts
+++ src/setup-node.ts
@@ -5,34 +5,36 @@ import * as path from 'path';
 
 async function run() {
   try {
-    //
-    // Version is optional.  If supplied, install / use from the tool cache
-    // If not supplied then task is still used to setup proxy, auth, etc...
-    //
-    let version = core.getInput('version');
-    if (!version) {
-      version = core.getInput('node-version');
-    }
-    if (version) {
-      // TODO: installer doesn't support proxy
-      await installer.getNode(version);
-    }
+    ////
+    //// Version is optional.  If supplied, install / use from the tool cache
+    //// If not supplied then task is still used to setup proxy, auth, etc...
+    ////
+    //let version = core.getInput('version');
+    //if (!version) {
+    //  version = core.getInput('node-version');
+    //}
+    //if (version) {
+    //  // TODO: installer doesn't support proxy
+    //  await installer.getNode(version);
+    //}
 
-    const registryUrl: string = core.getInput('registry-url');
-    const alwaysAuth: string = core.getInput('always-auth');
-    if (registryUrl) {
-      auth.configAuthentication(registryUrl, alwaysAuth);
-    }
+    //const registryUrl: string = core.getInput('registry-url');
+    //const alwaysAuth: string = core.getInput('always-auth');
+    //if (registryUrl) {
+    //  auth.configAuthentication(registryUrl, alwaysAuth);
+    //}
 
-    // TODO: setup proxy from runner proxy config
-    const matchersPath = path.join(__dirname, '..', '.github');
-    console.log(`##[add-matcher]${path.join(matchersPath, 'tsc.json')}`);
-    console.log(
-      `##[add-matcher]${path.join(matchersPath, 'eslint-stylish.json')}`
-    );
-    console.log(
-      `##[add-matcher]${path.join(matchersPath, 'eslint-compact.json')}`
-    );
+    //// TODO: setup proxy from runner proxy config
+    //const matchersPath = path.join(__dirname, '..', '.github');
+    //console.log(`##[add-matcher]${path.join(matchersPath, 'tsc.json')}`);
+    //console.log(
+    //  `##[add-matcher]${path.join(matchersPath, 'eslint-stylish.json')}`
+    //);
+    //console.log(
+    //  `##[add-matcher]${path.join(matchersPath, 'eslint-compact.json')}`
+    //);
+    const msg = core.getInput('msg');
+    console.log('msg:', msg);
   } catch (error) {
     core.setFailed(error.message);
   }
